b37d3b90d98885a1b6ba169cd0c07dbb2250176a
Daylon Wilkins <daylon@liquidata.co>
2020-05-06 14:25:04 -0700

    Can now skip x86 MSI creation if the zip does not exist

--- process.bat
+++ process.bat
@@ -5,15 +5,14 @@ SET PATH=%PATH%;.\wix
 :: Check that the files and directories exist
 if not exist "archives" mkdir "archives"
 if not exist "output" mkdir "output"
-if not exist "archives\dolt-windows-386.zip" echo Could not find "dolt-windows-386.zip" in archives & exit /B
 if not exist "archives\dolt-windows-amd64.zip" echo Could not find "dolt-windows-amd64.zip" in archives & exit /B
 :: Cleanup any previous output files
 del /F /Q "output\dolt-windows-386.msi" 2> nul
 del /F /Q "output\dolt-windows-amd64.msi" 2> nul
 echo ------------------------- Checking Dolt Version --------------------------
 :: Extract to a top-level dolt directory
-7z.exe x -oarchives archives/dolt-windows-386.zip > nul
-ren "archives\dolt-windows-386" dolt > nul
+7z.exe x -oarchives archives/dolt-windows-amd64.zip > nul
+ren "archives\dolt-windows-amd64" dolt > nul
 move "archives\dolt" dolt > nul
 :: Verify the version pulled
 for /f "tokens=*" %%i in ('dolt\bin\dolt.exe version') do set PROCESSED_DOLT_VERSION=%%i
@@ -23,23 +22,7 @@ if ERRORLEVEL 2 exit /B
 powershell -Command "(gc dolt-windows-386.wxs) -replace 'PROCESSED_DOLT_VERSION', '%PROCESSED_DOLT_VERSION%' | Out-File -encoding ASCII dolt-windows-386-proc.wxs"
 powershell -Command "(gc dolt-windows-amd64.wxs) -replace 'PROCESSED_DOLT_VERSION', '%PROCESSED_DOLT_VERSION%' | Out-File -encoding ASCII dolt-windows-amd64-proc.wxs"
 echo.
-echo ---------------------------- Creating x86 MSI ----------------------------
-:: Compile the MSI
-candle dolt-windows-386-proc.wxs > nul
-light -ext WixUIExtension -cultures:en-us dolt-windows-386-proc.wixobj
-ren dolt-windows-386-proc.msi dolt-windows-386.msi > nul
-move dolt-windows-386.msi output > nul
-:: Cleanup
-del /F /Q dolt-windows-386-proc.wxs 2> nul
-del /F /Q dolt-windows-386-proc.wixpdb 2> nul
-del /F /Q dolt-windows-386-proc.wixobj 2> nul
-rmdir /Q /S dolt > nul
-echo.
 echo ---------------------------- Creating x64 MSI ----------------------------
-:: Extract to a top-level dolt directory
-7z.exe x -oarchives archives/dolt-windows-amd64.zip > nul
-ren "archives\dolt-windows-amd64" dolt > nul
-move "archives\dolt" dolt > nul
 :: Compile the MSI
 candle dolt-windows-amd64-proc.wxs > nul
 light -ext WixUIExtension -cultures:en-us dolt-windows-amd64-proc.wixobj
@@ -49,4 +32,22 @@ move dolt-windows-amd64.msi output > nul
 del /F /Q dolt-windows-amd64-proc.wxs 2> nul
 del /F /Q dolt-windows-amd64-proc.wixpdb 2> nul
 del /F /Q dolt-windows-amd64-proc.wixobj 2> nul
+rmdir /Q /S dolt > nul
+:: Skip x86 if it does not exist
+if not exist "archives\dolt-windows-386.zip" exit /B
+echo.
+echo ---------------------------- Creating x86 MSI ----------------------------
+:: Extract to a top-level dolt directory
+7z.exe x -oarchives archives/dolt-windows-386.zip > nul
+ren "archives\dolt-windows-386" dolt > nul
+move "archives\dolt" dolt > nul
+:: Compile the MSI
+candle dolt-windows-386-proc.wxs > nul
+light -ext WixUIExtension -cultures:en-us dolt-windows-386-proc.wixobj
+ren dolt-windows-386-proc.msi dolt-windows-386.msi > nul
+move dolt-windows-386.msi output > nul
+:: Cleanup
+del /F /Q dolt-windows-386-proc.wxs 2> nul
+del /F /Q dolt-windows-386-proc.wixpdb 2> nul
+del /F /Q dolt-windows-386-proc.wixobj 2> nul
 rmdir /Q /S dolt > nul
